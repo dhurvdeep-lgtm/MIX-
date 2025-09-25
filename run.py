62a21257041137491cade8bd73769e8efbf144b6
dhurvdeep-lgtm <dhurvdeep@gmail.com>
2025-09-25 11:11:55 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,15 @@ import time
 import traceback
 from bot import CommandBot, appstate
 
+def start_bot():
+    print("✅ Bot starting...")
+    client = CommandBot(None, None, session_cookies=appstate)
+    client.listen()
+
 while True:
     try:
-        print("✅ Starting bot...")
-        client = CommandBot(None, None, session_cookies=appstate)
-        client.listen()
+        start_bot()
     except Exception as e:
-        print("❌ Bot crashed! Restarting in 10s...")
+        print("❌ Bot crashed, restarting in 10s...")
         traceback.print_exc()
         time.sleep(10)
